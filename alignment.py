fc57dd9ddf2acb5208ec5d2fc09a46cff00448bc
Groza Cristian <kristi9524@gmail.com>
2016-06-04 22:21:00 -0400

    Working global traceback.

--- alignment.py
+++ alignment.py
@@ -50,20 +50,20 @@ def GlobalAlign(i, j, s1, s2):
     # corner cases of V(0, j) and V(i, 0) are not properly handled in the
     # maximizing list
     if j != 0 and i == 0:
-        return V(i, j -1, s1, s2) + d
+        return GlobalAlign(i, j -1, s1, s2) + d
     elif i != 0 and j == 0:
-        return V(i - 1, j, s1, s2) + d
+        return GlobalAlign(i - 1, j, s1, s2) + d
     # Base case.
     if i == 0 and j == 0:
         return 0
     # Maximization list of all the recursive paths
-    return max([V(i - 1, j - 1, s1, s2) + sub[b2i[s1[i]]][b2i[s2[j]]],
-                V(i, j - 1, s1, s2) + d,
-                V(i - 1, j, s1, s2) + d])
+    return max([GlobalAlign(i - 1, j - 1, s1, s2) + sub[b2i[s1[i]]][b2i[s2[j]]],
+                GlobalAlign(i, j - 1, s1, s2) + d,
+                GlobalAlign(i - 1, j, s1, s2) + d])
 
 # Matrix method, most efficient
 def GlobalAlignMatrix(i, j, s1, s2):
-    # j is row number, i is column number
+    # j is row number (s2), i is column number (s1)
     matrix = [[0 for x in range(len(s1))] for y in range(len(s2))]
     # Fill first horizontal row
     for k in range(0, i + 1):
@@ -79,21 +79,31 @@ def GlobalAlignMatrix(i, j, s1, s2):
                                  matrix[k][z - 1] + d])
     return matrix
 
-def GlobalTraceBack(matrix):
-    j = len(matrix) - 1             # number of rows
-    i = len(matrix[0]) - 1          # number of columns
-    trace = [(j, i)]                # initial point
-    while i != 0 and j !=0:
-        # Remove i, j < 0, accept i, j => 0
-        cells = filter (lambda x : False if (x[0] < 0 and x[1] >= 0) or (x[0] >= 0 and x[1] < 0) else True
-                        ,[(j - 1, i - 1), (j - 1, i), (j, i - 1)])
-        values = map(lambda c: matrix[c[0]][c[1]], cells)
-        bestNeighbour = max (zip (cells, values), key = lambda x: x[1])
-        # Set new i, j values
-        j = bestNeighbour[0][0]
-        i = bestNeighbour[0][1]
-        trace.append(bestNeighbour[0]) # only add the (j, i) value
-    return trace
+def GlobalTraceBack(i, j, s1, s2, matrix):
+    s1Aligned = ""
+    s2Aligned = ""
+    while i > 0 and j > 0:
+       if matrix[j][i] == matrix[j - 1][i - 1] + sub[b2i[s1[i]]][b2i[s2[j]]]:
+           s1Aligned += s1[i]
+           s2Aligned += s2[j]
+           j -= 1;
+           i -= 1;
+       elif matrix[j][i] == matrix[j][i - 1] + d:
+           s1Aligned += s1[i]
+           s2Aligned += " "
+           i -= 1
+       elif matrix[j][i] == matrix[j - 1][i] + d:
+           s1Aligned += " "
+           s2Aligned += s2[j]
+           j -= 1
+    while i > 0:
+        s1Aligned += s1[i]
+        i -= 1
+    while j > 0:
+        s2Aligned += s2[j]
+        j -= 1
+    return (s1Aligned[::-1], s2Aligned[::-1])
+
 
 # Creates visual visualization of V(i, j) values for all pairs of V(i, j)
 def CreateMatrix(seq1, seq2):
@@ -113,13 +123,21 @@ def PrintMatrix(m):
 # Since sequences are indexed from 1 ... and strings are indexed from 0
 # ... spaces are added to realign the sequence indexes with the string
 # indices.
-def FindGlobalAligScore(s1, s2):
+def FindGlobalAlignScore(s1, s2):
     return GlobalAlign(len(s1), len(s2), " " + s1, " " + s2)
 
 def FindGlobalAlignMatrixScore(s1, s2):
-    j = len(s2)
     i = len(s1)
-    return GlobalAlignMatrix(i, j, " " + s1, " " + s2)
+    j = len(s2)
+    return GlobalAlignMatrix(i, j, " " + s1, " " + s2)[j][i]
+
+def FindBestAlignment(s1, s2):
+    i = len(s1)
+    j = len(s2)
+    s1_ = " " + s1
+    s2_ = " " + s2
+    m = GlobalAlignMatrix(i, j, s1_, s2_)
+    return GlobalTraceBack(i, j, s1_, s2_, m)
 
 def FindLocalAlignScore(s1, s2):
     return LocalAlign(len(s1), len(s2), " " + s1, " " + s2)
@@ -127,3 +145,5 @@ def FindLocalAlignScore(s1, s2):
 def FindLocalAlignMatrixScore(s1, s2):
     # Flatten list and return maximum value in matrix
     return max([i for row in LocalAlignMatrix(len(s1), len(s2), " " + s1, " " + s2) for i in row])
+
+print FindBestAlignment(T1, T2)
