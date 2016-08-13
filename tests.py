532b78ae9d91e8161dc224b4466824a86554434d
Groza Cristian <kristi9524@gmail.com>
2016-08-12 22:53:17 -0400

    org

--- tests.py
+++ tests.py
@@ -1,16 +1,17 @@
 from alignment import *
 
-# Test sequences
-T1 = "GAATC"
-T2 = "CATAC"
+def doTests():
+    # Test sequences
+    T1 = "GAATC"
+    T2 = "CATAC"
 
-print "S1: " + T1
-print "S2: " + T2
+    print "S1: " + T1
+    print "S2: " + T2
 
-# Alignment
-a = Alignment(T1, T2)
+    # Alignment
+    a = Alignment(T1, T2)
 
-# Global alignment test
-print "Global alignment: " + str(a.GlobalTraceback())
-# Local alignment test
-print "Local alignment: " + str(a.LocalTraceback())
+    # Global alignment test
+    print "Global alignment: " + str(a.GlobalTraceback())
+    # Local alignment test
+    print "Local alignment: " + str(a.LocalTraceback())
